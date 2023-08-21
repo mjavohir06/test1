11294cf91fffc2a184211e0c52ce6a87d8d198b0
mjavohir06 <136838733+mjavohir06@users.noreply.github.com>
2023-08-21 17:20:38 +0500

    Parametr
    p

--- asosiy/Bottomtab.jsx
+++ asosiy/Bottomtab.jsx
@@ -10,7 +10,8 @@ const Tab=createBottomTabNavigator()
 
 import React, { useEffect, useState } from 'react'
 import Home from '../components/Home';
-import Search from '../components/Search';
+
+import Profile from '../components/Profile';
 
 
 const Bottomtab = () => {
@@ -19,7 +20,7 @@ const Bottomtab = () => {
     <NavigationContainer theme={DarkTheme}>
       <Tab.Navigator>
         <Tab.Screen name='Home' options={{tabBarIcon(p){return <AntDesign name='home' {...p}/>}}} component={Home}/>
-        <Tab.Screen name='tarjimon' options={{tabBarIcon(p){return <MaterialIcons name='g-translate' {...p}/>}}} component={Search}/>
+        <Tab.Screen name='Sozlamalar' options={{tabBarIcon(p){return <AntDesign name='setting' {...p}/>}}} component={Profile}/>
       </Tab.Navigator>
     </NavigationContainer>
   )
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,13 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import LocalStore from "sync-storage"
+import { TextInput } from 'react-native'
+import { useState } from 'react'
 
 const Profile = () => {
+  const [q,setq]=useState('easy')
+  
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View>
+        <TextInput
+        
+        />
+      </View>
+      <Text style={styles.text}>Versiya: 1.0.0</Text>
+      <Text style={{...styles.text,color:"green"}}>Dasturchi :  Ma'murov Javohir</Text>
     </View>
   )
 }
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    marginLeft:15
+  },
+  text:{
+    width:"auto",
+    height:35,
+    marginTop:2,
+    marginBottom:2,
+    fontSize:23,
+    color:"white"
+  }
+})
 
 export default Profile
 
